5f80660cba0925bb94727498bbffc41b1ec60a83
Verseck <rafaverseck@gmail.com>
2023-04-04 12:13:56 -0300

    ok

---
+++ exs-jvs-2/ex-jvs-2.js
@@ -0,0 +1,22 @@
+
+const numero = parseInt(prompt("Digite sua nota"))
+
+
+const numero2 = parseInt(prompt("Digite sua nota"))
+
+const media =( numero + numero2)/2 
+
+alert("sua media é"+media)
+
+if(media>=7)
+{alert ("voce foi aprovado")}
+
+if(media<=7)
+{alert ("voce foi reprovado")}
+
+if(media==10)
+{alert ("voce foi aprovado com distinção")}
+
+
+
+ 
